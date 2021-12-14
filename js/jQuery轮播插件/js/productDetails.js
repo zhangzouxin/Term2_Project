aa499ffb782fd12dfe96a282c95750bdcf827570
zhangzouxin <3454164237@qq.com>
2021-12-14 15:28:49 +0800

    这是第九周的项目文件

---
+++ js/jQuery轮播插件/js/productDetails.js
@@ -0,0 +1,9 @@
+$(function() {
+    magnifier({
+        magnifier: "#magnifier1", //最外层的大容器
+        width: 340, //图片放大后承载容器宽度
+        height: 470, //图片放大后承载容器高度
+        zoom: 4, //缩放比例
+    })
+
+})
